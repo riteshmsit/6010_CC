46d362a0094858cdbaf30ae0b212ffb5124a77e5
riteshmsit <riteshveluvolurockz@msitprogram.net>
2019-04-07 22:03:07 +0530

    module 7

--- m7/Assignment 4/reducer.py
+++ m7/Assignment 4/reducer.py
@@ -4,31 +4,13 @@
 from operator import itemgetter
 import sys
 
-genes = {}
-
-
-# input comes from STDIN
+strings = []
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-    words = line.split(", ")
-
-    words[0] = words[0].strip("[")
-    words[0] = words[0].strip("\"")
-
-    words[1] = words[1].strip("]")
-    words[1] = words[1].strip("\"")
-    
-    if words[0] not in genes.keys():
-        genes[words[0]] = words[1][0:-10]
-
-# print genes
-key_list = list(genes.keys())
-val_list = list(genes.values())
-
-updated_val = set(val_list)
-for i in updated_val:
-    print "\""+i+"\""
-# print len(updated_val)
-
-
+    line = line.strip("\"")
+    length = len(line)
+    length = length - 10
+    line = line[0: length]
+    if line not in strings:
+        strings.append(line)
+        print "%s" % line
