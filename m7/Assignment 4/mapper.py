44b972fec43622f86c324fc3d6b62ca751616122
riteshmsit <riteshveluvolurockz@msitprogram.net>
2019-04-07 21:38:07 +0530

    module 7

--- m7/Assignment 4/mapper.py
+++ m7/Assignment 4/mapper.py
@@ -3,12 +3,9 @@
 
 import sys
 
-
-# input comes from STDIN (standard input)
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    # words = line.split(", ")
-    # increase counters
-    print line
+	line = line.strip()
+	line = line.strip("[")
+	line = line.strip("]")
+	data = line.split(", ")
+	print '%s' % data[1]
