2a6e437f1f96c5f6594eb2fdf6a5476204a44a94
riteshmsit <riteshveluvolurockz@msitprogram.net>
2019-04-07 22:01:14 +0530

    module 7

--- m7/Assignment 3/reducer.py
+++ m7/Assignment 3/reducer.py
@@ -1,53 +1,23 @@
+    
 #!/usr/bin/env python
 """reducer.py"""
 
 from operator import itemgetter
 import sys
 
-friends_dict = {}
-
-
-# input comes from STDIN
+relations = []
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
-    words = line.split(", ")
-
-    words[0] = words[0].strip("[")
-    words[0] = words[0].strip("\"")
-
-    words[1] = words[1].strip("]")
-    words[1] = words[1].strip("\"")
-    
-    if words[0] not in friends_dict.keys():
-        friends_dict[words[0]] = [words[1]]
+    data = line.split('\t', 1)
+    normaldata = [data[0], data[1]]
+    if normaldata not in relations:
+        relations.append(normaldata)
     else:
-        friends_dict[words[0]].append(words[1])
-
-
-# print friends_dict
-
-
-dict2 = friends_dict.copy()
-
-# print "After update"
-
-li = friends_dict.keys()
-
-# print li
-
-# print "----------------------"
-
-for i in li:
-    for j in friends_dict[i]:
-        if j in li:
-            if i in friends_dict[j]:
-                friends_dict[j].remove(i) 
-                friends_dict[i].remove(j)
-
-# print friends_dict
-# print "***********"
-for k in friends_dict.keys():
-    for l in friends_dict[k]:
-        print [l, k]
-        print [k ,l]      
+        relations.remove(normaldata)
+    reversedata = [data[1], data[0]]
+    if reversedata not in relations:
+        relations.append(reversedata)
+    else:
+        relations.remove(reversedata)
+for relation in relations:
+    print '%s' % relation
